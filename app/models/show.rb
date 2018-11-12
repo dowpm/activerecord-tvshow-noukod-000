405d0c7c3e254491716dc824e831448f1a38c7f1
Mac Dowell Prosper <macdowell@noukod.com>
2018-11-12 19:08:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,9 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    where(rating: highest_rating)[0]
+    where(rating: highest_rating).first
+  end
+
+  def self.lowest_rating
   end
 end
